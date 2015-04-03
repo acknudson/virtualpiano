c768999521e5b52d06d96d524d930be8d133c0d4
Aubrey Colter <ajcolter@gmail.com>
2015-04-02 20:21:34 -0400

    added a fingerSprite class for the gui

--- src/gui.py
+++ src/gui.py
@@ -9,7 +9,7 @@ fingerDots = [(70,80), (85,80), (100,80), (115,80), (130,80)]
 
 # initialize pygame and the screen
 pygame.init()
-screen = pygame.display.set_mode((800, 300))
+screen = pygame.display.set_mode((800, 300)) # width, heigh values
 pygame.display.set_caption("LeaPiano")
 
 
@@ -23,8 +23,29 @@ def init():
 	screen.blit(background, (0,0))
 
     # initialize basic GUI
-	screenSize = screen.get_size()
+	screenSize = screen.get_size() # (width, height)
 	pianoLine = pygame.draw.line(background, RED, (10,screenSize[1]/2), (screenSize[0]-10, screenSize[1]/2))
+
+	# create finger sprites for each finger, put into left and right groups
+	rthumb = fingerSprite()
+	rindex = fingerSprite()
+	rmiddle = fingerSprite()
+	rring = fingerSprite()
+	rpinky = fingerSprite()
+
+
+	rightHandSprites = pygame.sprite.Group()
+	rightHandSprites.add(pygame.sprite.Group(), rthumb, rindex, rmiddle, rring, rpinky)
+
+	lthumb = fingerSprite()
+	lindex = fingerSprite()
+	lmiddle = fingerSprite()
+	lring = fingerSprite()
+	lpinky = fingerSprite()
+
+
+	leftHandSprites = pygame.sprite.Group()
+	leftHandSprites.add(lthumb, lindex, lmiddle, lring, lpinky)
 
 	pygame.display.update() #this is crucial -- writes the values to the screen
 
@@ -37,5 +58,21 @@ def update(play): #rename play to something more descriptive - it's all the gest
 			color = RED
 		pygame.draw.circle(background, color, fingerDots[i], 2)
 
-	screen.blit(background, (0,0)) #erase screen (return to basic background)
-	pygame.display.flip() #redraw with new updates
+	screen.blit(background, (0,0)) #erase screen (return to basic background) NEED THIS LINE
+	pygame.display.flip() # redraw with *new* updates (similar to pygame.display.update())
+
+
+class fingerSprite(pygame.sprite.Sprite): #may want to use the dirty sprite class for better rendering? 
+	#https://www.pygame.org/docs/ref/sprite.html
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		# self.x = x
+		# self.y = y
+		# self.z = z
+		self.image = pygame.Surface([3,3]) #hardcoded the width and height values of the sprite's image
+		self.image.fill(BLUE) #hardcoded the color of the sprite
+		self.rect = self.image.get_rect()
+
+	def update(self, x, y, z):
+		self.rect.x = x
+		self.rect.y = y
